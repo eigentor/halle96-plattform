c96f7b6ab4136aebd35bbfbde45d950fe222bdf5
Thomas Moseler <tmoseler@gmx.de>
2014-05-21 15:06:26 +0200

    Basis-Theme auf Grundlage von Responsive Bartik erstellt

--- sites/all/themes/halle96/config/compass.rb
+++ sites/all/themes/halle96/config/compass.rb
@@ -7,6 +7,9 @@ sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
+require "breakpoint"
+require "singularitygs"
+
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
