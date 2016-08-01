316a0f3d759f0ba4defd32fd925f82d6592654dc
Thomas Moseler <tmoseler@gmx.de>
2016-08-01 17:50:26 +0200

    Jquery Validation Plugin

--- sites/all/modules/custom/halle_96_timepicker/js/halle96_timepicker.js
+++ sites/all/modules/custom/halle_96_timepicker/js/halle96_timepicker.js
@@ -2,7 +2,10 @@
   Drupal.behaviors.halle96_timepicker = {
     attach: function (context, settings) {
 
-
+      $('#conference-room-node-form #edit-field-conf-termin-und-0-value-timeEntry-popup-1')
+        .add('#conference-room-node-form #edit-field-conf-termin-und-0-value2-timeEntry-popup-1').timepicker({
+          showPeriodLabels: false,
+      });
 
 
     } // end of attach function
