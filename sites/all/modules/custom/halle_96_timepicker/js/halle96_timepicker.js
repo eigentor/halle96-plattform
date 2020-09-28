3ec0547b1da4432af2b43b07f02467d639bee3e3
Thomas Moseler <tmoseler@gmx.de>
2020-09-28 15:07:22 +0200

    Timepicker auch in bezahlten Konferenzräumen verwenden. Geht noch nicht...

--- sites/all/modules/custom/halle_96_timepicker/js/halle96_timepicker.js
+++ sites/all/modules/custom/halle_96_timepicker/js/halle96_timepicker.js
@@ -3,7 +3,12 @@
     attach: function (context, settings) {
 
       $('#conference-room-node-form #edit-field-conf-termin-und-0-value-timeEntry-popup-1')
-        .add('#conference-room-node-form #edit-field-conf-termin-und-0-value2-timeEntry-popup-1').timepicker({
+        .add('#conference-room-node-form #edit-field-conf-termin-und-0-value2-timeEntry-popup-1')
+        .add('#conference-room-paid-1-node-form #edit-field-conf-termin-und-0-value-timeEntry-popup-1')
+        .add('#conference-room-paid-1-node-form #edit-field-conf-termin-und-0-value2-timeEntry-popup-1')
+        .add('#conference-room-paid-2-node-form #edit-field-conf-termin-und-0-value-timeEntry-popup-1')
+        .add('#conference-room-paid-2-node-form #edit-field-conf-termin-und-0-value2-timeEntry-popup-1')
+        .timepicker({
           showPeriodLabels: false,
       });
 
